e4c061d056b868d1f492c3f8d4521fcd93069a5d
Ginger Pearson & Joshua Storey <student@epicodus.com>
2014-07-22 14:57:01 -0700

    add test for else return num and elsif to return 5

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,9 @@
+def fizz_buzz(num)
+  if num % 3 == 0
+    "fizz"
+  elsif num % 5 == 0
+    "buzz"
+  else
+    num
+  end
+end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,7 +2,15 @@ require('rspec')
 require('fizz_buzz')
 
 describe('fizz_buzz') do
-  it('should return fizz if it is divisible by 3') do
+  it('should return fizz if inputted number is divisible by 3') do
     fizz_buzz(3).should(eq("fizz"))
+  end
+
+  it('should return "num" if number is not divisible by 3') do
+    fizz_buzz(7).should(eq(7))
+  end
+
+  it('should return buzz if the number is divisible by 5') do
+    fizz_buzz(5).should(eq("buzz"))
   end
 end
